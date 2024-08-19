ddaa366608bf818d68afd77f604fa20f0086bb78
aizaz-dev <azazkhan2682000@gmail.com>
2024-08-20 02:20:51 +0500

    corousel

--- src/app/components/Slider/Slider.jsx
+++ src/app/components/Slider/Slider.jsx
@@ -5,25 +5,29 @@ import SliderTwo from "./SliderTwo";
 
 const Slider = () => {
   return (
-<div className=" w-full bg-[#FFFFFF]">
-  <div className=" w-[100%] max-w-[1200px] mx-auto px-[16px] sm:px-[80px] sm:py-[80px] py-[40px]">
-<div className=" flex items-center flex-col gap-4">
-  <button
-className=" w-fit Inter bg-[#F4D800] pt-[6px] pr-[60px] pb-[6px] pl-[60px] Inter text-[#000000] sm:text-[23px] text-[19px] font-[500] text-center border-solid border-0 border-t-2 border-r-2 border-b-2  border-l-2 border-[#ffd500] rounded-tl-[50px] rounded-br-[50px]"  >
-Sharpen Your Edge
-  </button>
+    <div className=" w-full bg-[#FFFFFF]">
+      <div className=" w-[100%] max-w-[1200px] mx-auto px-[16px] sm:px-[80px] sm:py-[80px] py-[40px]">
+        <div className=" flex items-center flex-col gap-4">
+          <button className=" w-fit Inter bg-[#F4D800] pt-[6px] pr-[60px] pb-[6px] pl-[60px] Inter text-[#000000] sm:text-[23px] text-[19px] font-[500] text-center border-solid border-0 border-t-2 border-r-2 border-b-2  border-l-2 border-[#ffd500] rounded-tl-[50px] rounded-br-[50px]">
+            Sharpen Your Edge
+          </button>
 
-  <p className=" text-center text-[#000000] sm:text-[40px] text-[26px] font-[700] Inter">
-Join the ranks of our satisfied customers
-  </p>
-</div>
+          <p className=" text-center text-[#000000] sm:text-[40px] text-[26px] font-[700] Inter">
+            Join the ranks of our satisfied customers
+          </p>
+        </div>
 
-<SliderOne />
-<SliderTwo />
-<div className=" w-full py-24"><img src="/Samurai/bg-foot.png" alt="" className=" w-full object-cover object-center "/></div>
-  </div>
-
-</div>
+        <SliderOne />
+        <SliderTwo />
+        <div className=" w-full py-24">
+          <img
+            src="/Samurai/bg-foot.png"
+            alt=""
+            className=" w-full object-cover object-center "
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/app/components/Slider/SliderOne.jsx
+++ src/app/components/Slider/SliderOne.jsx
@@ -1,24 +1,123 @@
-import React from 'react';
-import Marquee from 'react-fast-marquee';
+'use client';
+import React, { useState } from 'react';
+import Slider from 'react-slick';
+import { FaCaretRight, FaCaretLeft } from 'react-icons/fa';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const SliderOne = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const images = [
+    '/Samurai/img3.png',
+    '/Samurai/img4.png',
+    '/Samurai/img6.png',
+    '/Samurai/img7.png',
+    '/Samurai/img8.png',
+    '/Samurai/3.png',
+
+  ];
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    draggable: true,
+    nextArrow: <FaCaretRight />,
+    prevArrow: <FaCaretLeft />,
+    responsive: [
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  };
+
+  const openModal = (index) => {
+    setCurrentIndex(index);
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const prevImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
+
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  };
+
   return (
-    <Marquee
-      speed={30}
-      direction="right"
-      pauseOnHover={true}
-      gradientColor={[255, 213, 0]}
-      className='flex mt-[40px]'
-    >
-      
-      <img className='w-[100px] sm:w-[120px] md:w-[140px] xl:w-[188px] h-auto pr-[50px]' src="/Samurai/img2.png" alt="alt" />
-      <img className='w-[100px] sm:w-[120px] md:w-[140px] xl:w-[188px] h-auto pr-[50px]' src="/Samurai/img3.png" alt="alt" />
-      <img className='w-[100px] sm:w-[120px] md:w-[140px] xl:w-[188px] h-auto pr-[50px]' src="/Samurai/img4.png" alt="alt" />
-      <img className='w-[100px] sm:w-[120px] md:w-[140px] xl:w-[188px] h-auto pr-[50px]' src="/Samurai/img6.png" alt="alt" />
-      <img className='w-[100px] sm:w-[120px] md:w-[140px] xl:w-[188px] h-auto pr-[50px]' src="/Samurai/img7.png" alt="alt" />
-      <img className='w-[100px] sm:w-[120px] md:w-[140px] xl:w-[188px] h-auto pr-[50px]' src="/Samurai/img8.png" alt="alt" />
-      
-    </Marquee>
+    <>
+      <div className="relative mt-[40px]">
+        <Slider {...settings}>
+          {images.map((src, index) => (
+            <div 
+              key={index}
+              className="px-2 flex justify-center items-center"
+            >
+              <img
+                className="h-[150px] max-md:m-auto p-[20px] object-contain cursor-pointer"
+                src={src}
+                alt={`Image ${index + 1}`}
+                onClick={() => openModal(index)}
+              />
+            </div>
+          ))}
+        </Slider>
+        <div className="text-center mt-4 text-white">
+          {`${currentIndex + 1} of ${images.length}`}
+        </div>
+      </div>
+
+      {isOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
+          <button
+            className="absolute left-4 text-white text-3xl"
+            onClick={prevImage}
+          >
+            <FaCaretLeft />
+          </button>
+          <div className="relative">
+            <img
+              className="max-w-[90%] max-h-[90%] object-contain"
+              src={images[currentIndex]}
+              alt={`Image ${currentIndex + 1}`}
+            />
+            <div className="absolute right-0 -bottom-7 text-white text-sm bg-opacity-50 px-2 py-1">
+              {`${currentIndex + 1} of ${images.length}`}
+            </div>
+          </div>
+          <button
+            className="absolute right-4 text-white text-3xl"
+            onClick={nextImage}
+          >
+            <FaCaretRight />
+          </button>
+          <button
+            className="absolute top-4 right-4 text-white text-3xl"
+            onClick={closeModal}
+          >
+            &times;
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
